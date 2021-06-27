517f514d5d7eac8bd0bdadc4451fee62a10af9c4
hibaancode <86377117+hibaancode@users.noreply.github.com>
2021-06-27 12:35:36 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ var dog, happyDog, database, foodS, foodStock;
 function preload()
 {
   //load images here
-  dog1 = loadImage("images/dogImg.png")
-  happyDog = loadImage("images/dogImg1.png")
+  dog1 = loadImage("dogImg.png")
+  happyDog = loadImage("dogImg1.png")
 }
 
 function setup() {
